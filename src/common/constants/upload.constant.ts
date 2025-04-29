f6614164764ede62e4cf0bb247f70387cb2adc89
ramazanismayilov <ramazanismayilovh@gmail.com>
2025-04-29 17:04:49 +0400

    test

--- src/common/constants/upload.constant.ts
+++ src/common/constants/upload.constant.ts
@@ -1,6 +1,6 @@
 export const UPLOAD_MEDIA_MAX_SIZE = 1e8;
 export const UPLOAD_IMAGE_ALLOWED_MIME_TYPES = ['image/jpeg', 'image/png', 'image/webp'];
 export const UPLOAD_IMAGE_ALLOWED_TYPES = ['jpeg', 'jpg', 'png', 'webp'];
-
+ 
 export const UPLOAD_VIDEO_ALLOWED_MIME_TYPES = ['video/mp4', 'video/webm', 'video/ogg'];
 export const UPLOAD_VIDEO_ALLOWED_TYPES = ['mp4', 'webm', 'ogg'];
