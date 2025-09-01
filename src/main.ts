df3159c5512604f2d514b62f43aabea7fc605303
ramazanismayilov <ramazanismayilovh@gmail.com>
2025-09-01 17:20:02 +0400

    cors problem solve

--- src/main.ts
+++ src/main.ts
@@ -3,48 +3,49 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-let cachedApp: any = null;
-
 async function bootstrap() {
-  if (!cachedApp) {
-    const app = await NestFactory.create(AppModule);
-
-    app.setGlobalPrefix('api');
-    app.enableCors();
-
-    app.useGlobalPipes(
-      new ValidationPipe({
-        transform: true,
-        whitelist: true,
-        forbidNonWhitelisted: true,
-      }),
-    );
-
-    const config = new DocumentBuilder()
-      .setTitle('Epic Games')
-      .setDescription('The Epic Games API description')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build();
-
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
-
-    // 🚀 Swagger-i kökdə ("/") aç
-    SwaggerModule.setup('/', app, documentFactory, {
-      swaggerOptions: {
-        persistAuthorization: true,
-      },
-    });
-
-    await app.init(); // listen yoxdur, Vercel üçün init
-    cachedApp = app;
-  }
-
-  return cachedApp;
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'Origin',
+      'X-Requested-With',
+      'Access-Control-Request-Method',
+      'Access-Control-Request-Headers'
+    ],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  });
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+  
+  const config = new DocumentBuilder()
+    .setTitle('Epic Games')
+    .setDescription('The Epic Games API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+    
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true
+    }
+  });
+  
+  await app.listen(process.env.PORT ?? 3000);
 }
 
-export default async function handler(req, res) {
-  const app = await bootstrap();
-  const server = app.getHttpAdapter().getInstance();
-  return server(req, res);
-}
+bootstrap();
